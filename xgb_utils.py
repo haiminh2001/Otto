6626641257a3a13a6ea6a455111f079776336775
haiminh2001 <haiminhnguyen2001@gmail.com>
2023-01-02 01:04:54 +0000

    rank + prod, not so succeed

--- xgb_utils.py
+++ xgb_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-FEATURES = ['cofitness_cosub', 'cofitness_time_decay', 'num_appearance', 'type_weight', 'coclick', 'cocart', 'coorder']
+FEATURES = ['cofitness_cosub', 'cofitness_time_decay', 'num_appearance', 'num_cosub', 'coclick', 'cocart', 'coorder']
 USER_FEATURES = ['num_sub', 'consistency', 'num_actions', 'pr']
 ITEM_FEATURES = ['num_clicks', 'num_carts', 'num_orders', 'pr']
 # INTERACTION_FEATURES = ['user_clicks', 'user_carts', 'user_orders']
@@ -18,53 +18,52 @@ feature_id_map = {
 }
 
 def create_data(infer_data, infer = True):
-
   
-
-  data =  {
-      'user': infer_data[:,0].astype(np.int32),
-      'item': infer_data[:, 1].astype(np.int32),
-      'type': infer_data[:,2].astype(np.uint16),
-
-      
-  }
-
-  for f in FEATURES: 
-    data[f] = infer_data[:, len(data.keys())]
-
-
   aggs = ['mean', 'var']
   
+  agg_features_name = []
+  quo_features_name = []
+  prod_features_name = []
   
   for f in FEATURES:
     for agg in aggs: 
-      data[f + '_' + agg] = infer_data[:, len(data.keys())].astype(float)
-
-  
-
-  for f in USER_FEATURES: 
-    f = f if f != 'pr' else 'user_pr'
-    data[f] = infer_data[:, len(data.keys())]
-
-  for f in ITEM_FEATURES: 
-    f = f if f != 'pr' else 'item_pr'
-    data[f] = infer_data[:, len(data.keys())]
-  
+      agg_features_name.append(f + '_' + agg)
+      quo_features_name.append('qou_' + f + '_' + agg)
+      
   for i in range(len(FEATURES)):
     for j in range(i + 1, len(FEATURES)):
-      data['prod_' + FEATURES[i] + '_' + FEATURES[j]] = infer_data[:, len(data.keys()) ]
+      prod_features_name.append('prod_' + FEATURES[i] + '_' + FEATURES[j]) 
+  
+  columns = [
+              *FEATURES,
+             *[f if f != 'pr' else 'item_pr' for f in ITEM_FEATURES],
+             *quo_features_name,  
+             *prod_features_name,
+             ]
+  
+  
 
-  candidates = pd.DataFrame(data)
+  rank_features_name = ['rank_' + f for f in columns]
+  columns = ['user', 'item', 'type',*columns, 
+             *rank_features_name,
+             *agg_features_name,
+             *[f if f != 'pr' else 'user_pr' for f in USER_FEATURES],]
+  
+
+
+  assert len(columns) == infer_data.shape[1], (len(columns), infer_data.shape[1])
+
+
+  candidates = pd.DataFrame(data = infer_data, columns = columns, copy=False)
   candidates['clicks'] = np.where(candidates['type'] == 0, 1, 0)
   candidates['carts'] = np.where(candidates['type'] == 1, 1, 0)
   candidates['orders'] = np.where(candidates['type'] == 2, 1, 0)
-  assert candidates['clicks'].sum() + candidates['carts'].sum() + candidates['orders'].sum() == candidates.shape[0]
   if infer:
     t =  candidates['type'].values
     del candidates['type']
     return candidates, t
   else:
-    candidates ['item'] = candidates['item'] - 100000000
+    # candidates ['item'] = candidates['item'] - 100000000
     return candidates
 
 
