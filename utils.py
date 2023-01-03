c1ebbe3693417e2d47d07bf3c7a54701c8417838
haiminh2001 <haiminhnguyen2001@gmail.com>
2023-01-03 04:00:25 +0000

    add dirty trick but not help

---
+++ utils.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+def get_score(df, session_list, split = False):
+    submission = df.copy()
+    test_labels = pd.read_parquet('/home/anhphantq/otto/splitted_data/test_labels.parquet')
+    test_labels = test_labels[(test_labels['session'] <= np.amax(session_list)) & (test_labels['session'] >= np.amin(session_list))]
+    
+    submission['session'] = submission.session_type.apply(lambda x: int(x.split('_')[0]))
+    
+    session = set(test_labels['session'].unique().tolist())
+    for j in session_list:
+      assert j in session
+
+
+    submission['type'] = submission.session_type.apply(lambda x: x.split('_')[1])
+    
+    if split:
+      submission.labels = submission.labels.apply(lambda x: [int(i) for i in x.split(' ')[:20]])
+
+    test_labels = test_labels.merge(submission, how='left', on=['session', 'type'])
+    test_labels['hits'] = test_labels.apply(lambda df: len(set(df.ground_truth).intersection(set(df.labels))), axis=1)
+    test_labels['gt_count'] = test_labels.ground_truth.str.len().clip(0,20)
+
+    del submission
+
+    recall_per_type = test_labels.groupby(['type'])['hits'].sum() / test_labels.groupby(['type'])['gt_count'].sum() 
+
+    print (f"Score : {(recall_per_type * pd.Series({'clicks': 0.10, 'carts': 0.30, 'orders': 0.60})).sum()}")
+    return recall_per_type
+
+
+def shuffle(l):  
+    perm = np.random.permutation(len(l))  
+    l[:] = [l[j] for j in perm]  
+    return perm
+
+def unshuffle(L, perm):  
+    assert len(L[0]) == len(perm), (len(L[0]), len(perm))
+    res = [[None] * len(l) for l in L]
+    for i, j in enumerate(perm):
+        for k, l in enumerate(L):
+          res[k][j] = l[i]
+          
+    for i, l in enumerate(L):
+      l[:] = res[i]
+      
+
+def create_submission(clicks, carts, orders, session_list, MAX_SESSION, JOIN):
+    click_new_append = [' '.join([str(int(i) - MAX_SESSION) for i in a]) for a in clicks] if JOIN else [[int(j) - MAX_SESSION for j in i] for i in clicks]
+    cart_new_append = [' '.join([str(int(i) - MAX_SESSION) for i in a]) for a in carts] if JOIN else [[int(j) - MAX_SESSION for j in i] for i in carts]
+    order_new_append = [' '.join([str(int(i) - MAX_SESSION) for i in a]) for a in orders] if JOIN else [[int(j) - MAX_SESSION for j in i] for i in orders]  
+    submission1 = pd.DataFrame({'session_type': session_list, 'labels': click_new_append})
+    submission2 = pd.DataFrame({'session_type': session_list, 'labels': cart_new_append})
+    submission3 = pd.DataFrame({'session_type': session_list, 'labels': order_new_append})
+    submission1['session_type'] = submission1['session_type'].apply(lambda x: str(x) + '_clicks')
+    submission2['session_type'] = submission2['session_type'].apply(lambda x: str(x) + '_carts')
+    submission3['session_type'] = submission3['session_type'].apply(lambda x: str(x) + '_orders')
+    final = pd.concat([submission1, submission2, submission3])
+    del submission1, submission2, submission3
+    return final
--- xgb_utils.py
+++ xgb_utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from utils import unshuffle
+import xgboost as xgb
 
 FEATURES = ['cofitness_cosub', 'cofitness_time_decay', 'num_appearance', 'num_cosub', 'coclick', 'cocart', 'coorder']
 USER_FEATURES = ['num_sub', 'consistency', 'num_actions', 'pr']
@@ -7,11 +9,13 @@ USER_FEATURES = ['num_sub', 'consistency', 'num_actions', 'pr']
 
 item_features = ['num_clicks', 'num_carts', 'num_orders', 'pr']
 recent_features = []
+dirty_features = []
 for i in range(7,0,-1):
     for j in range(3):
         recent_features.append(f'recent_day{i}_type{j}')
+        dirty_features.append(f'future_day{i}_type{j}')
 
-ITEM_FEATURES = [*item_features, * recent_features]
+ITEM_FEATURES = [*item_features, * recent_features, * dirty_features]
 
 INTERACTION_FEATURES = ['user_clicks', 'user_carts', 'user_orders', 'user_num_sub', 'user_time_decay', 'user_lts', 'user_fts']
 
@@ -30,7 +34,12 @@ recent_features_id_map = dict(zip(
   recent_features, [len(feature_id_map) + i for i in range(len(recent_features))]
 ))
 
-feature_id_map = {**feature_id_map, **recent_features_id_map}
+dirty_features_id_map = dict(zip(
+  dirty_features, [len(feature_id_map) + len(recent_features_id_map) + i for i in range(len(dirty_features))]
+))
+
+
+feature_id_map = {**feature_id_map, **recent_features_id_map, **dirty_features_id_map}
 
 def create_data(infer_data, infer = True):
   
@@ -63,13 +72,12 @@ def create_data(infer_data, infer = True):
     del candidates['type']
     return candidates, t
   else:
-    # candidates ['item'] = candidates['item'] - 100000000
     return candidates
 
 
 
 
-def create_test_data(infer_data, infer = True):
+def create_test_data(infer_data, infer = True, max_session = None):
 
 
     
@@ -84,7 +92,7 @@ def create_test_data(infer_data, infer = True):
   candidates = pd.DataFrame(data = infer_data, columns = columns, copy=False)
  
   if not infer:
-    candidates ['item'] = candidates['item'] - 100000000
+    candidates ['item'] = candidates['item'] - max_session
   return candidates
 
 
@@ -102,3 +110,50 @@ def get_len_group(idx, num_cands):
   assert np.sum(groups) == n, (np.sum(groups), n)
   return groups
 
+
+def xgboost_infer(infer_data, perm, models):
+  df_infer_data, np_type = create_data(infer_data)
+  preds = np.zeros(df_infer_data.shape[0])
+  dtest = xgb.DMatrix(data=df_infer_data.iloc[:, 2:])
+
+  for model in models:
+
+      preds += model.predict(dtest) / len(models)
+      
+  predictions = df_infer_data[['user','item']].copy()
+  predictions['type'] = np_type
+  predictions['pred'] = preds
+
+  predictions = predictions.sort_values(['user', 'pred'], ascending=[True, False]).reset_index(drop=True)
+  predictions['n'] = predictions.groupby('user').item.cumcount().astype('int8')
+  predictions = predictions.loc[predictions.n<20]
+  sub = predictions.groupby('user').item.apply(list)
+  sub = sub.to_frame().reset_index()
+  items = sub.item.values.tolist()
+  users = sub.user.values
+  unshuffle([items, users], perm)
+  return items, users
+
+
+def xgboost_test_infer(infer_data, perm, models):
+  df_infer_data = create_test_data(infer_data)
+  preds = np.zeros(df_infer_data.shape[0])
+  dtest = xgb.DMatrix(data=df_infer_data.iloc[:, 2:])
+
+  for model in models:
+
+      preds += model.predict(dtest) / len(models)
+      
+  predictions = df_infer_data[['user','item']].copy()
+  predictions['pred'] = preds
+
+  predictions = predictions.sort_values(['user', 'pred'], ascending=[True, False]).reset_index(drop=True)
+  predictions['n'] = predictions.groupby('user').item.cumcount().astype('int8')
+  predictions = predictions.loc[predictions.n<20]
+  sub = predictions.groupby('user').item.apply(list)
+  sub = sub.to_frame().reset_index()
+  items = sub.item.values.tolist()
+  users = sub.user.values
+  unshuffle([items, users], perm)
+  return items, users
+    
